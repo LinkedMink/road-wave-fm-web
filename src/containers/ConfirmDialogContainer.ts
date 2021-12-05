0d329f736c4937dbc2c3946ce4d2a002aded4cc8
Harlan Sang <harlansang@gmail.com>
2021-12-05 00:17:35 -0600

    chore: Update packages and add git files

--- src/containers/ConfirmDialogContainer.ts
+++ src/containers/ConfirmDialogContainer.ts
@@ -9,13 +9,16 @@ import { confirmSetValue } from '../actions/ConfirmAction';
 import { RootState } from '../reducers/RootReducer';
 import { Dispatch } from 'redux';
 
-const mapStateToProps: MapStateToProps<ConfirmDialogStateProps, ConfirmDialogOwnProps, RootState> =
-  (state: RootState) => {
-    return {
-      isActive: state.confirm.active ? true : false,
-      text: state.confirm.active ? state.confirm.active.message : undefined,
-    };
+const mapStateToProps: MapStateToProps<
+  ConfirmDialogStateProps,
+  ConfirmDialogOwnProps,
+  RootState
+> = (state: RootState) => {
+  return {
+    isActive: state.confirm.active ? true : false,
+    text: state.confirm.active ? state.confirm.active.message : undefined,
   };
+};
 
 const mapDispatchToProps: MapDispatchToPropsFunction<
   ConfirmDialogDispatchProps,
