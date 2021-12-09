b036c5dec0b695ad8d899f48881c208ae32ae43f
Harlan Sang <harlansang@gmail.com>
2021-12-09 17:57:55 -0600

    feat: Add develop halt comment and build script

--- README.md
+++ README.md
@@ -1,31 +1,41 @@
 # Road Wave FM - Web App
 
-## Overview
-Work in Progress
+- ![Build State](https://github.com/LinkedMink/road-wave-fm-web/actions/workflows/verify-build.yml/badge.svg)
 
-https://roadwavefm.linkedmink.space/
+## Overview
+
+Development on this project has stalled in favor of focusing on a mobile app front-end
+[road_wave_fm_ui](https://github.com/LinkedMink/road_wave_fm_ui). We wanted a UI that would be convenient to use
+while driving, but the integration of web apps on mobile devices isn't the greatest. If someone's interested in
+picking this project up feel free to reach out.
 
 ## Dependencies
+
 - [node-user-service](https://github.com/LinkedMink/node-user-service)
 - [road-wave-fm-api](https://github.com/LinkedMink/road-wave-fm-api)
 - MongoDB ^4.0 (for API dependencies)
 
 ## Deployment
+
 This package creates static files that can be served by any web server. Some tested options:
 
 ### Kubernetes Bare Metal
+
 - Serving Files: Docker Nginx Image
 - Orchestration with Dependencies: Kubernetes Bare-Metal (amd64 and arm64 images)
 - Database: MongoDB Bare-Metal Replica Set
 
 #### Environment Config
+
 - deploy/build-docker.sh
 - deploy/kube-deploy.yaml
 
 ### AWS
+
 - Serving Files: AWS Amplify
 - Orchestratio with Dependencies : AWS EKS (amd64 and arm64 images)
 - Database: AWS MongoDB Atlas
 
 #### Environment Config
+
 - deploy/aws.amplify.env
--- src/components/FooterPanel.tsx
+++ src/components/FooterPanel.tsx
@@ -9,11 +9,19 @@ import Box from '@material-ui/core/Box';
 import Link from '@material-ui/core/Link';
 import Typography from '@material-ui/core/Typography';
 
-type StyleClass = 'footerBox';
+type StyleClass = 'footerBox' | 'footerText';
 
-const styles: StyleRulesCallback<Theme, Record<string, never>, StyleClass> = (_theme: Theme) => ({
+const styles: StyleRulesCallback<Theme, Record<string, never>, StyleClass> = (theme: Theme) => ({
   footerBox: {
+    marginLeft: theme.spacing(4),
+    display: 'flex',
+    flexDirection: 'row',
     padding: 0,
+  },
+  footerText: {
+    padding: theme.spacing(0, 4),
+    borderLeft: `1px solid ${theme.palette.divider}`,
+    borderRight: `1px solid ${theme.palette.divider}`,
   },
 });
 
@@ -23,13 +31,24 @@ class FooterPanel extends React.Component<FooterPanelProps> {
   render() {
     return (
       <Box pt={4} className={this.props.classes?.footerBox}>
-        <Typography variant="body1" color="textSecondary" align="center">
+        <Typography
+          variant="body1"
+          color="textSecondary"
+          className={this.props.classes?.footerText}
+        >
           {'Copyright © '}
           <Link color="inherit" href="#">
             Harlan Sang
           </Link>{' '}
           {new Date().getFullYear()}
           {'.'}
+        </Typography>
+        <Typography
+          variant="body1"
+          color="textSecondary"
+          className={this.props.classes?.footerText}
+        >
+          <Link href="docs/privacy_policy.md">Privacy Policy</Link>
         </Typography>
       </Box>
     );
